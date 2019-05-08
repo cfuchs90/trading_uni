56f36ab44422bc0ba02e1ddc5f8984c2a9125438
foxmasterc <fuchs.christian90@gmail.com>
2019-05-08 22:43:05 +0200

    Optimization seems to be working

--- donchian.R
+++ donchian.R
@@ -10,38 +10,44 @@ initDate <- "1960-01-01"
 startDate <- "1960-01-02"
 endDate <- "2019-01-01"
 
-getSymbols("^GDAXI", from = startDate, to = endDate)#, from = startDate, to = endDate, adjusted = TRUE)
-stock("GDAXI", currency="USD", multiplier = 1)
-colnames(GDAXI) <- c("Open", "High", "Low", "Close", "Volume", "Adjusted")
-GDAXI <- na.omit(GDAXI)
-#av_tr <- ATR(GDAXI, n = 20)
-# bla <- DonchianChannel(HL = GDAXI[, 1:2], n = 20)
+getSymbols("^GSPC", from = startDate, to = endDate)#, from = startDate, to = endDate, adjusted = TRUE)
+stock("GSPC", currency="USD", multiplier = 1)
+colnames(GSPC) <- c("Open", "High", "Low", "Close", "Volume", "Adjusted")
+GSPC <- na.omit(GSPC)
+#av_tr <- ATR(GSPC, n = 20)
+# bla <- DonchianChannel(HL = GSPC[, 1:2], n = 20)
 # Set up initial equity and transaction costs
 start_equity <- 1e6
 orderSize <- start_equity * 0.02
-fees = 2 # round trip fee of 2$
+fee = 2 # Transaction fee of $2
 stopp_loss <- 0.02
 
+init_n <- 20
+n_opt_range <- 1:200
 
 
 Sys.setenv(TZ="UTC")
 
-# set up the strategy and portfolio components
 
-rm.strat(donchian_strategy)
 donchian_strategy <- "donchian_channel"
 
+# set up the strategy and portfolio components
+rm.strat(donchian_strategy)
 
-initPortf(donchian_strategy, "GDAXI", initDate = initDate)
-initAcct(donchian_strategy, portfolios = donchian_strategy, initDate = initDate, initEq = start_equity)
-initOrders(portfolio = donchian_strategy, initDate = initDate)
 strategy(donchian_strategy, store = TRUE)
+initPortf(donchian_strategy, "GSPC", initDate = initDate)
+initAcct(donchian_strategy,  portfolios = donchian_strategy,
+         initDate = initDate, initEq = start_equity,
+         currency = 'USD')
+
+initOrders(donchian_strategy, initDate = initDate)
+
 
 # Create the indicator
 add.indicator(strategy = donchian_strategy,
               name = "DonchianChannel",
               arguments = list(HL = quote(HLC(mktdata)[, 1:2]),
-                               n = 20,
+                               n = init_n,
                                include.lag = TRUE
                                ),
               label = "DNC")
@@ -118,43 +124,47 @@ add.rule(donchian_strategy, name = "ruleSignal",
              orderside = 'short'),
          type = 'exit'
          )
-## add.rule(donchian_strategy, name = "ruleSignal",
-
-##          arguments = list(
-##              sigcol = "long",
-##              sigval = TRUE,
-##              orderqty = "all",
-##              ordertype = "stoplimit",
-##              orderside = "long",
-##              tmult = TRUE,
-##              threshold = stopp_loss
-##          ),
-##          type = "exit",
-##          label = "ExitLong"
-##          )
-
-## # Exit Short
-## add.rule(donchian_strategy, name = "ruleSignal",
-##          arguments = list(
-##              sigcol = "short",
-##              sigval = TRUE,
-##              orderqty = "all",
-##              ordertype = "stoplimit",
-##              orderside = "short",
-##              tmult = TRUE,
-##              threshold = stopp_loss
-##          ),
-##          type = "exit",
-##          label = "ExitShort"
 
 results <- applyStrategy(donchian_strategy, portfolios = donchian_strategy)
-getTxns(Portfolio=donchian_strategy, Symbol="GDAXI")
-chart.Posn(donchian_strategy, Symbol = "GDAXI", Dates = "2017::")
+getTxns(Portfolio=donchian_strategy, Symbol="GSPC")
+chart.Posn(donchian_strategy, Symbol = "GSPC", Dates = "2017::")
 
 updatePortf(donchian_strategy)
 updateAcct(donchian_strategy)
 updateEndEq(donchian_strategy)
-## chart.Posn(donchian_strategy, Symbol = 'GDAXI', Dates = '1998::')
+chart.Posn(donchian_strategy, Symbol = 'GSPC', Dates = '2005::')
 
-trade_stats <- perTradeStats(donchian_strategy,"GDAXI")
+trade_stats <- perTradeStats(donchian_strategy,"GSPC")
 
+
+# Optimize the moving average parameter
+
+add.distribution(donchian_strategy,
+                 paramset.label = 'DonchianChannel',
+                 component.type = 'indicator',
+                 component.label = 'DNC',
+                 variable = list(n = n_opt_range),
+                 label = 'days_opt')
+
+library(parallel)
+detectCores()
+
+if( Sys.info()['sysname'] == "Windows" )
+{
+    library(doParallel)
+    registerDoParallel(cores=detectCores())
+} else {
+    library(doMC)
+    registerDoMC(cores=detectCores())
+}
+
+
+
+optimization <- apply.paramset(donchian_strategy,
+  paramset.label='DonchianChannel',
+  portfolio.st=donchian_strategy,
+  account.st=donchian_strategy, nsamples=0)
+
+tradeResults <- optimization$tradeStats
+idx <- order(tradeResults[,1], tradeResults[,2])
+tradeResults <- tradeResults[idx,]
