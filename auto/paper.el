73204137317e57b4d177b66e6805436fed72726c
Christian Fuchs <fuchs.christian90@gmail.com>
2019-06-13 18:31:33 +0200

    Fixed all figure and table references

--- auto/paper.el
+++ auto/paper.el
@@ -6,15 +6,15 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "lat_pre"
@@ -34,40 +34,43 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:orgd7ad9fe"
+    "sec:org4cbfc83"
     "sec:intro"
-    "sec:org351d9ea"
+    "sec:org3ceaa94"
     "img:stock"
     "eq:calmar-ratio"
-    "sec:org94b1789"
-    "sec:org80a51a2"
+    "sec:org09300e2"
+    "sec:org0ba86de"
     "sec:buyhold"
-    "sec:org297c05b"
+    "sec:orgd5644d4"
     "sec:donchian"
-    "sec:org2ff1ff4"
-    "sec:org426c9f0"
-    "sec:org8758bf5"
-    "sec:orgdb6f93c"
+    "sec:orgc2a7789"
+    "sec:org120b4cb"
+    "sec:org8be4efc"
+    "sec:org9d2562b"
     "img:buyhold-strategy"
     "img:buyhold-performance"
     "tab:buyhold-trading-statistics"
     "img:buyhold-portfolio-summary"
-    "sec:org23dd689"
+    "sec:org01371ae"
     "sec:strategy"
     "img:donchian-strategy"
     "img:donchian-performance"
     "tab:strategy-statistics"
-    "sec:org0ed6584"
+    "sec:org9c2b015"
     "sec:comparison"
+    "sec:orgc8010b8"
     "img:relative-performance"
+    "sec:org2f1d434"
     "img:donchian-vs-buyhold"
+    "sec:org18995e9"
     "tab:both-statistics"
-    "sec:org67bb956"
+    "sec:org4316b45"
     "tab:ff-regression"
     "img:strategy_summary"
-    "sec:org3a25766"
-    "sec:org61575aa"
-    "sec:org3583e97"
-    "sec:orgaede4f1"))
+    "sec:org3536642"
+    "sec:orgef36af9"
+    "sec:orga63156e"
+    "sec:org1d8649f"))
  :latex)
 
